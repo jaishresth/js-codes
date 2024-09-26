f3bcb1bf3489ee9fb2dd2f345a9db912076d8939
Jai Shresth <jayshresth333@gmail.com>
2024-09-26 07:28:09 +0000

    data types

---
+++ 01_basics/02_variables.js
@@ -0,0 +1,27 @@
+"use stict";//treat all JS code as newer version
+
+//alert("hello"); //we are using node js, not browser
+
+//console.log(3+
+    
+    //3)// code readability should be high
+
+    console.log("hello")
+
+    let name = "Jai"
+    let age = 18
+    let isLoggedIn = false
+    let state;
+
+    //number => 2 to power 53
+    //bigint
+    //string => ""
+    //boolean => true/false
+    //null => standalone value
+    // undefined =>
+    // symbol => unique
+
+    //object
+
+    console.log(typeof null); //object
+    console.log(typeof undefined); //undefined
